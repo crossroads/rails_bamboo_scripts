e45c89f9d1471910624c3d21bd63e5d23b532131
Nathan Broadbent <nathan.f77@gmail.com>
2011-05-20 03:14:49 +0800

    Organized scripts

--- install.sh
+++ install.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 base_path=/opt/rails_bamboo_scripts/
-ln -fs $base_path/setup_rvm_environment       /usr/bin
-ln -fs $base_path/test_db_config              /usr/bin
-ln -fs $base_path/yum_bamboo_packages         /usr/bin
-ln -fs $base_path/keep_bamboo_agent_alive     /usr/bin
-ln -fs $base_path/update_rails_bamboo_scripts /usr/bin
-ln -fs $base_path/bamboo_heartbeat.cron       /etc/cron.d/bamboo_heartbeat.cron
+ln -fs $base_path/bin/*      /usr/bin
+ln -fs $base_path/cron/*     /etc/cron.d/
 
