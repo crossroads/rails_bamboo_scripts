f89f685515e4c86600fb390e7791cf6df7379cab
Nathan Broadbent <nathan.f77@gmail.com>
2011-05-20 03:07:20 +0800

    Added bamboo heartbeat cron task

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 base_path=/opt/rails_bamboo_scripts/
-ln -fs $base_path/setup_rvm_environment       /usr/bin/setup_rvm_environment
-ln -fs $base_path/test_db_config              /usr/bin/test_db_config
-ln -fs $base_path/yum_bamboo_packages         /usr/bin/yum_bamboo_packages
-ln -fs $base_path/update_rails_bamboo_scripts /usr/bin/update_rails_bamboo_scripts
+ln -fs $base_path/setup_rvm_environment       /usr/bin
+ln -fs $base_path/test_db_config              /usr/bin
+ln -fs $base_path/yum_bamboo_packages         /usr/bin
+ln -fs $base_path/keep_bamboo_agent_alive     /usr/bin
+ln -fs $base_path/update_rails_bamboo_scripts /usr/bin
+ln -fs $base_path/bamboo_heartbeat.cron       /etc/cron.d/bamboo_heartbeat.cron
 
